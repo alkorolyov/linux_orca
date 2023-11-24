d6240b0aaa355bea7048a5cd9de3e4b788702315
Alexander Korolyov <49795502+alkorolyov@users.noreply.github.com>
2023-11-24 10:46:30 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,22 +9,23 @@ if [ ! -f Miniforge3-Linux-x86_64.sh ]; then
   wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh  -q --show-progress
 fi
 bash Miniforge3-Linux-x86_64.sh -b
-echo "source ~/miniforge3/etc/profile.d/conda.sh" >> ~/.bashrc
-echo "source ~/miniforge3/etc/profile.d/mamba.sh" >> ~/.bashrc
-source ~/.bashrc
+source ~/miniforge3/etc/profile.d/conda.sh
+source ~/miniforge3/etc/profile.d/mamba.sh
 rm Miniforge3-Linux-x86_64.sh
 conda config --set auto_activate_base false
+conda config --set ssl_verify false
 
 # create env
 mamba env create --name $env_name --file environment.yml
 conda activate $env_name
 
 pip config set global.trusted-host "pypi.org files.pythonhosted.org pypi.python.org"
+pip install cclib
 
 # molli
-wget https://github.com/SEDenmarkLab/molli_firstgen/archive/refs/heads/main.zip -O molli.zip
-pip install molli.zip #molli package
-rm molli.zip
+# wget https://github.com/SEDenmarkLab/molli_firstgen/archive/refs/heads/main.zip -O molli.zip -q --show-progress
+# pip install molli.zip #molli package
+# rm molli.zip
 
 # install as a local copy for dev
 pip install -e .
